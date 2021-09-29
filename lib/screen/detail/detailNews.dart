5b9978a10fba097de22041c6a770244a15daf5d9
shinoj-exe <shinojmuralee@gmail.com>
2021-09-30 00:48:10 +0530

    new Page

--- lib/screen/detail/detailNews.dart
+++ lib/screen/detail/detailNews.dart
@@ -6,7 +6,34 @@ class DetailsNews extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: MainBar()),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        actions: [Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(Icons.share,color: Colors.black,),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(Icons.share,color: Colors.black,),
+        )],
+        leading: IconButton(onPressed: (){
+          Navigator.of(context).pop();
+        },icon: Icon(Icons.arrow_back_ios),color: Colors.black,),
+      ),
+      body: SafeArea(child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              MainBar(),
+              const SizedBox(height: 10,),
+              Text("Data"),
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
